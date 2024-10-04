660263e84a25e6e87abb9473e1377c1782abb4b9
apah6 <meng9940@naver.com>
2024-10-04 18:37:30 +0900

    v0.6 dataType

--- week06/main.go
+++ week06/main.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	//f := 12.9
-	var f float64 = 12.9
-	c1 := 'Z'
-	c2 := '김'
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Println(c1, c2)    //10진 표현
-	fmt.Printf("%x\n", c2) //유니코드 '김' 16진수 변환 유니코드 사이트:https://www.unicode.org/
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i)
+
+	i = 3
+	f = 2.7
+	fmt.Print("\n\n", f <= float64(i), "\n")
 }
