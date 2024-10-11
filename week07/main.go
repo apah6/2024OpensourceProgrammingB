aacc894312294403921c507501bcde0d1187ed17
apah6 <meng9940@naver.com>
2024-10-11 17:04:25 +0900

    v0.8strings

---
+++ week07/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var army string = "우리는 !가와 !민에 충성을 다하는 대한민! 육군이다."
+	armyFixed := strings.NewReplacer("!", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
+}
