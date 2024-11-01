68b72454513fc416e4c3a289874e092344025bd0
apah6 <meng9940@naver.com>
2024-11-01 18:26:15 +0900

    V2.8 Prime number

--- week10/main.go
+++ week10/main.go
@@ -9,9 +9,30 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+
+		i := 3
+
+		for i*i <= n {
+			if n%i == 0 {
+				return false
+			}
+			fmt.Printf("%d ", i)
+			i = i + 2
+		}
+	}
+	return true
+}
+
 func main() {
 	fmt.Print("정수 입력: ")
-	//fmt.Printf("%f\n", math.Sqrt(25.0))
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
 
@@ -26,33 +47,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//count := 0
-
-	var isPrime bool = true //int -> bool counts -> isprime 메모리효율증가
-
-	if n <= 1 {
-		isPrime = false //가독성
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 { //2를 제외한 모든짝수는 소수가 아님
-		isPrime = false
-	} else { //3 이상의 홀수
-
-		i := 3
-
-		//for i < n {
-		//for i <= int(math.Sqrt(float64(n))) {
-		for i*i <= n {
-			if n%i == 0 {
-				isPrime = false
-				break // 1과 자기 자신을 제외한 첫 번째 약수가 발견 되면 반복문 종료
-			}
-			fmt.Printf("%d ", i)
-			i = i + 2
-		}
-	}
-	//if count == 0 {
-	if isPrime { //비교연산 제거
+	if isPrime(n) {
 		fmt.Printf("%d는(은) 소수입니다.\n", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다!\n", n)
