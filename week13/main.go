39054bcbf2c53d8876d11f7893b9bc9e1a72f695
apah6 <meng9940@naver.com>
2024-11-22 17:03:49 +0900

    V4.0

--- week13/main.go
+++ week13/main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	var emptySlice []bool
+	//var emptySlice = make([]bool,5)
+	fmt.Printf("%#v\n", emptySlice)
+
 	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
 	gpas_Slice := gpas[1:4]
 	//gpas_Slice[1] = 2.71
