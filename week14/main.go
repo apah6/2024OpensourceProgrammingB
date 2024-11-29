9d85a95cd0668add1e0a75b83f0af6ddb520c9ec
apah6 <meng9940@naver.com>
2024-11-29 17:50:20 +0900

    V4.4 struct

--- week14/main.go
+++ week14/main.go
@@ -2,26 +2,21 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student struct {
-		id   int
-		name string
-		gpa  float32
-	}
-
-	student.id = 202444049
-	student.name = "Maeng Eunjae"
-	student.gpa = 3.5
-	fmt.Println(student.gpa)
-
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
-
+	var student1 student
+	student1.id = 202444049
+	student1.name = "Maeng Eunjae"
+	student1.gpa = 3.5
+	fmt.Println(student1.gpa)
+	var student2 student
 	student2.id = 202444040
-	student2.name = "Unkwon"
+	student2.name = "Unknown"
 	student2.gpa = 3.6
 	fmt.Println(student2.gpa)
 }
